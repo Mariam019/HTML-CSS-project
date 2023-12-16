cfbb4c92b50b1e08dd25d048cf63d02afc2e8038
Mariam019 <mariamzaoui1960@gmail.com>
2023-12-17 00:35:20 +0100

    first commit

---
+++ Fluffy's _To_Do_List.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>Fluffy's To Do List</title>
+        <link  href="photos/productivity(1).png" rel="icon" />
+
+       
+        <style>
+            @font-face {
+   font-family:Dancing_Script ;
+   src: url(fonts/Dancing_Script/DancingScript-VariableFont_wght.ttf);
+}
+
+h1 {
+   font-family: Dancing_Script;
+   font-size: 80px;
+}
+
+
+body{
+    background-color:  #D9C4EC;
+}
+.container{
+    background-color: #ffffff;
+    width: 50%;
+    top: 40%;
+    left: 50%;
+    border-radius: 10px;
+    min-width: 450px;
+   
+    min-height: 100px;
+    
+}
+#newtask{
+    position: relative;
+    padding: 30px 20px;
+}
+#newtask input{
+    width: 75%;
+    height: 45px;
+    padding: 12px;
+    color: #111111;
+    font-weight: 500;
+    position: relative;
+    border-radius: 5px;
+    font-family: 'Dancing_Script',sans-serif;
+    font-size: 18px;
+    border: 2px solid #d1d3d4;
+}
+#newtask input:focus{
+    outline: none;
+    border-color: #DE3163;
+}
+#newtask button{
+    
+    font-weight: 500;
+    font-size: 16px;
+    background-color: #E37383;
+    border-color: #DE3163;
+    color: #ffffff;
+    cursor: pointer;
+    outline: none;
+    width: 20%;
+    height: 45px;
+    border-radius: 5px;
+    font-family: 'Dancing_Script';
+    top:-60px;
+    right: -10px;
+}
+
+
+.task span{
+    font-family: 'Dancing_Script';
+    font-size: 25px;
+    font-weight: 400;
+    font-size: 18px;
+}
+.list-item input[type="checkbox"] {
+
+position: absolute;
+right: 0;
+
+}
+.completed {
+    text-decoration: line-through;
+    
+}
+
+   
+.pic{
+    
+    position: fixed;
+      top: 260px;
+      right: 5px;
+      border-radius: 10%;
+    
+}
+.quote{
+    position: fixed;
+      top: 500px;
+      right: 5px;
+      font-size: 18px;
+}
+.insc{
+    position: fixed;
+    font-family: Dancing_Script;
+   font-size: 20px;
+   right:300px;
+   top:600px;
+}
+header {
+            background-color: #D9C4EC;
+            padding: 0px;
+            text-align: center;
+        }
+#clock {
+            border: 2px solid #cacaca;
+            padding: 10px;
+            background-color: #D9C4EC;
+            display: inline-block;
+            border-radius: 10%;
+           
+        }
+
+
+        </style>
+    </head>
+    <body>
+        <header>
+            <h2 id="clock"></h2>
+        </header>
+        
+            <h1>To Do List</h1>
+
+        <div class="container">
+            <div id="newtask">
+                <input type="text"  id="task" placeholder="Add Task"/>
+                <button onclick="addToDo()" id="push">Add New Task</button>
+            </div>
+            <ul id="tasks"></ul>
+        </div>
+
+       <div class="pic">
+            <img src="photos/応援-サポート.gif" style="width:400px;height:250px;" class="pic"/>
+        </div>
+
+            <div>
+                
+                <ul style=" font-family: 'Dancing_Script'" class="quote">
+                    <li>Stay productive.</li> 
+                    <li>You may delay, but time will not.</li>  
+                    <li>The key to productivity is to rotate your avoidance techniques.</li>  
+                  </ul>
+            </div>
+
+
+           <p>
+                <a class="insc" href="Inscription.php"><b>🌸 Inscription</b></a>
+            </p>
+           
+           <script language="javascript">
+            // Get references to the DOM elements
+let task = document.getElementById('task');
+let tasks = document.getElementById('tasks');
+
+// Function to add a new to-do item
+function addToDo() {
+ let todoText = task.value.trim();
+  
+ if (todoText.length > 0) {
+    let li = document.createElement('li');
+    li.textContent = todoText;
+    
+    let checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.onchange = function() {
+    li.classList.toggle('completed');
+
+};
+    li.appendChild(checkBox);
+    tasks.appendChild(li);
+    
+    // Clear the input field
+    task.value = '';
+ }
+}
+
+// Event listeners to handle adding to-do items
+task.addEventListener('keypress', function(event) {
+ if (event.key === 'Enter') {
+    addToDo();
+ }
+});
+
+
+function updateClock() {
+            var now = new Date();
+            var hours = now.getHours();
+            var minutes = now.getMinutes();
+            var seconds = now.getSeconds();
+            var time = hours + ":" + minutes + ":" + seconds;
+
+            document.getElementById("clock").textContent = time;
+
+            setTimeout(updateClock, 1000); // Update the clock every second
+        }
+
+        updateClock(); // Start the clock
+        </script>
+       
+    </body>
+</html>
---
+++ Inscription.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Inscription</title>
+        <link href="photos/inscription.png" rel="icon"/>
+
+        <style>
+              @font-face {
+   font-family:Dancing_Script ;
+   src: url(fonts/Dancing_Script/DancingScript-VariableFont_wght.ttf);
+}
+
+h1 {
+   font-family: 'Dancing_Script';
+   font-size: 30px;
+}
+body{
+    background-color:  #D9C4EC;
+}
+.email{
+    font-family: 'Dancing_Script';
+   font-size: 20px;
+   height: 45px;
+    border-radius: 5px;
+}
+.pass{
+    font-family: 'Dancing_Script';
+   font-size: 20px;
+   height: 45px;
+    border-radius: 5px;
+}
+.cancelbtn{
+    font-family: 'Dancing_Script';
+   font-size: 20px;
+   background-color: #FB7B8E;
+   font-weight: 500;
+    font-size: 16px;
+    width: 10%;
+    height: 45px;
+    border-radius: 5px;
+    cursor: pointer;
+}
+.signupbtn{
+    font-family: 'Dancing_Script';
+   font-size: 20px; 
+   background-color: #FB7B8E;
+   font-weight: 500;
+    font-size: 16px;
+    width: 10%;
+    height: 45px;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+        </style>
+    </head>
+    <body>
+        
+        <form id="Inscription" action="Inscription.php" method="post">
+            <h1>Sign up for more</h1>
+
+
+            <label class="email" for="email">Email :</label>
+            <input class="email" type="email" placeholder="Enter email" name="email"  required/>
+            <br></br>
+
+            <label class="pass" for="password">Password :</label>
+            <input class="pass" type="password" placeholder="******" name="password"  required/>
+            <br></br>
+
+            <label class="pass" for="confirm-password">Confirm password :</label>
+            <input class="pass" type="password" placeholder="******" name="confirm-password"  required/>
+            <br></br>
+
+            <label class="pass">
+                <input  type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+              </label>
+              <br></br>
+              <button type="button" onclick="" class="cancelbtn">Cancel</button>
+              <button type="submit" onclick="signup()" class="signupbtn">Sign Up</button>
+        </form>
+        <p id="message"></p>
+
+        <script language="javascript"> 
+        function signup(){ var form = document.getElementById("Inscription"); 
+        var formData = new FormData(form); 
+        var xhr = new XMLHttpRequest(); 
+        xhr.open("POST", "Inscription.php", true); 
+        xhr.onreadystatechange = function()
+        { if(xhr.readyState == 4 && xhr.status == 200)
+            { var result = JSON.parse(xhr.responseText); 
+                if(result.success)
+                { alert("User has been signed up successfully!"); 
+                window.location.href = "Fluffy's_To_Do_List.php"; //redirect to home page
+             }else
+             { document.getElementById("message").textContent = result.message; } } }
+              xhr.send(formData); } 
+        </script>
+        <?php
+header('Content-Type: text/html');
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "signup_form";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+$sql = "INSERT INTO form ( email, password) VALUES (?, ?)";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $email, $password);
+
+if($stmt->execute()){
+    echo json_encode(array("success" => true));
+}else{
+    echo json_encode(array("success" => false, "message" => "An error occurred during the sign up process. Please try again later."));
+}
+
+$stmt->close();
+$conn->close();
+?>
+    </body>
+</html>
